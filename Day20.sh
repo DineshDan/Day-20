912af4b31119ffd37035750a86bf0176c8d3faa0
DineshDan <dineshdanchai341@gmail.com>
2020-12-01 14:49:32 +0530

    Added: Logic to check the various methods

--- Day20.sh
+++ Day20.sh
@@ -1,2 +1,18 @@
-echo "Welcome to the program"
+ class A {
+	A () {
+	System.out.println("Program");
+		}
+	A(int i) {
+	   System.out.println(i);
+		}
+	A(String b , int c) {
+	   System.out.println(b);
+	   System.out.println(c);
+		}
+public static void main(String args[]) {
+	A a1 = new A();
+ 	A a2 = new A(100);
+	 A a3 = new A("dinesh" , 40);
+	  }
+}
 
